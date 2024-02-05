86c53044a8aada516e3bcc07b467429e0519016d
pavlovdog <pavlovdog.moscow@gmail.com>
2024-02-05 20:10:23 +0400

    Update cricuit
    - Tree size increased to 2^16

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const KEY_REGISTRY_ADD_EVENT_SIGNATURE = 'event Add( uint256 indexed fid, uint32 indexed keyType, bytes indexed key, bytes keyBytes, uint8 metadataType, bytes metadata )';
 
-export const MESSAGE_VERSION = 1;
+export const MESSAGE_VERSION = 2;
